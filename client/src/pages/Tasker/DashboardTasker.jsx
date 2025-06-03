7f492ca9247c06713d184a6b902a7007af9ec7b7
unknown <glennntoutoume8@.com>
2025-06-03 15:44:39 +0200

    v1 du backend

--- client/src/pages/Tasker/DashboardTasker.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DashboardTasker = () => {
-  return (
-    <div>DashboardTasker</div>
-  )
-}
-
-export default DashboardTasker
--- server/routes/task.routes.js
+++ server/routes/task.routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import { authorize, adminOnly } from '../middlewares/auth.middleware.js' // Décommenter et s'assurer que adminOnly est importé si besoin ailleurs
+import { authorize, adminOnly } from '../middlewares/auth.middleware.js'
 import { createTask, getTaskById, getTasks, getUserTasks, updateTask, deleteTask } from '../controllers/task.controller.js'
 
 const taskRouter = Router()
 
-taskRouter.get('/', authorize, adminOnly,getTasks) // Recuperer toutes les taches
+taskRouter.get('/', authorize, adminOnly, getTasks) // Recuperer toutes les taches
 
-taskRouter.get('/mytasks', authorize, getUserTasks) // Suggestion: route dédiée pour les tâches de l'utilisateur connecté
+taskRouter.get('/mytasks', authorize, getUserTasks) //  Recuperer les tâches de l'utilisateur connecté
 
 taskRouter.get('/:id',  authorize, getTaskById) // Recuperer le details d'une tache d'un utilisateur
 
